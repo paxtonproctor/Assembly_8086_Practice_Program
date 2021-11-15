c9d84fa73775a8fa21f9c4fb6c0b17ea0e3d5dd8
Paxton Proctor <pax.proctor@gmail.com>
2021-11-15 14:28:13 -0600

    cool

--- Assembly_Programs/Operations.asm
+++ Assembly_Programs/Operations.asm
@@ -1,9 +1,27 @@
 ; Paxton Proctor
 ; 11/12/2021
-; This program does a load of operations and prints them.
+; This program adds and subtracts 32 bit integers
+INCLUDE Irvine32.inc
 
 .MODEL SMALL
 .STACK 100H
 .DATA
+val1 DWORD 10000h
+val2 DWORD 40000h
+val3 DWORD 20000h
+finalVal DWORD?
 
 .CODE
+MAIN PROC
+
+    mov     ax,@data
+    mov     ds,ax
+
+    mov     eax,val1    ; start with 10000h
+    add     eax,val2    ; add 40000h
+    sub     eax,val3    ; subtract 20000h
+    mov     finalVal,eax    ; store the result(30000h)
+    call    DumpRegs    ; display the registers
+    exit
+main ENDP
+END main
